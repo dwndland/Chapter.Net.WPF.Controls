a1994518f24b35e107ac45572d2ca4b0e68dbe26
David Wendland <dwndland@gmail.com>
2024-05-20 15:59:48 +0200

    Add base classes for buttons and controls

---
+++ Chapter.Net.WPF.Controls/Bases/ControlBase.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------------------------------------------------
+// <copyright file="ControlBase.cs" company="my-libraries">
+//     Copyright (c) David Wendland. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------------------------------------------------
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Chapter.Net.WPF.Controls.Bases
+{
+    /// <summary>
+    ///     Base class for controls.
+    /// </summary>
+    public abstract class ControlBase : Control
+    {
+        /// <summary>
+        ///     Create a new instance of ControlBase.
+        /// </summary>
+        protected ControlBase()
+        {
+            Loaded += OnLoaded;
+            IsEnabledChanged += OnIsEnabledChanged;
+        }
+
+        /// <summary>
+        ///     Callback when the control got loaded.
+        /// </summary>
+        /// <param name="sender">The loaded checkbox.</param>
+        /// <param name="e">The loaded event parameter.</param>
+        protected virtual void OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        ///     Callback when the control IsEnabled got changed.
+        /// </summary>
+        /// <param name="sender">The checkbox.</param>
+        /// <param name="e">The IsEnabledChanged event parameter.</param>
+        protected virtual void OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+    }
+}
